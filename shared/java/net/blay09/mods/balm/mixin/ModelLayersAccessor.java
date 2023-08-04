63b830d340dedca5fbb5a64773e1e491e3f157fe
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2023-08-05 00:16:58 +0500

    v4.5.8: Attempt to fix Balm in dev environments
    This change is basically what it should do?
    Instead of throwing an AssertionError, now it returns a set of ModelLayerLocations.
    This should allow us to maintain support for Enhanced Workbenches and our other forks in a much easier way than before.

--- shared/java/net/blay09/mods/balm/mixin/ModelLayersAccessor.java
+++ shared/java/net/blay09/mods/balm/mixin/ModelLayersAccessor.java
@@ -6,13 +6,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mixin(ModelLayers.class)
 public interface ModelLayersAccessor {
-
     @Accessor("ALL_MODELS")
     static Set<ModelLayerLocation> getAllModels() {
-        throw new AssertionError();
+        return ModelLayers.getKnownLocations().collect(Collectors.toSet());
     }
-
 }
